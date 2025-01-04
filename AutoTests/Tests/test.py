cfc05428011824bf0951fbb1dd4786e9cc4caa4b
KadR94 <kadr94@gmail.com>
2025-01-04 14:04:59 +0300

    test.py

--- AutoTests/Tests/test.py
+++ AutoTests/Tests/test.py
@@ -16,5 +16,23 @@ class TestCalc:
         with pytest.raises(ZeroDivisionError):
             self.calc.division(self, 1, 0)
 
+    def test_multyply_success(self):
+        assert self.calc.multiply(self, 5, 5) == 25
+
+    def test_multyply_unseccess(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 2, 2) == 0
+
+    def test_subtraction_unsuccess(self):
+        assert self.calc.subtraction(self, 2, 2) == 1
+
+    def test_division_success(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_division_unsuccess(self):
+        assert self.calc.division(self, 2, 2) == 0
+
     def teardown(self):
         print('Выполнение метода Teardown')
